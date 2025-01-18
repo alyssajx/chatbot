35dab213bfd31909ce0a8ba0797144db36a64cd3
sanya <sanghavisanya@gmail.com>
2025-01-18 17:31:22 +0800

    Updated Embedding file and created the load_documents and split_text function in ingest.py file

--- embedding.py
+++ embedding.py
@@ -1,10 +1,10 @@
 from mistralai.client import MistralClient
 import numpy as np
 
-client = MistralClient(api_key = )
+client = st.secrets["MISTRAL_AI_KEY"]
 
 def embed(input: str):
     return client.embeddings("mistral-embed", input = input).data[0].embedding
 
 embeddings = np.array([embed(chunk) for chunk in chunks])
-dimension - embeddings.shape[1]
+dimension = embeddings.shape[1]
