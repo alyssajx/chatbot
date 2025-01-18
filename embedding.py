381c52e45f8bc99bbd547fc9eb041e7d5bdde555
alyssa <alyssacheong@u.nus.edu>
2025-01-18 18:10:21 +0800

    new embedding

--- embedding.py
+++ embedding.py
@@ -7,20 +7,11 @@ def embed(input: str):
     return client.embeddings("mistral-embed", input = input).data[0].embedding
 
 embeddings = np.array([embed(chunk) for chunk in chunks])
-dimension - embeddings.shape[1]
-
-#query
-question = input("Please ask a question:")
-question_embeddings = np.array([embed(question)])
-
-#retrieval 
-D, I = index.search(question_embeddings, k=2) 
-distance_threshold = 0.7
-def retrieval():
-    if D[0][0] > distance_threshold:
-        return "Sorry, I don't know the answer."
-    else: 
-        retrieved_chunk = [chunks[i] for i in I.tolist()[0]]
-        return retrieved_chunk
-        
 dimension = embeddings.shape[1]
+
+import faiss
+d = embeddings.shape[1]
+index = faiss.IndexFlatL2(d)
+index.add(embeddings)
+        
+
