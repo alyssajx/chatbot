0de2e9bc58d25c0d25b002c907ad2174804be305
alyssa <alyssacheong@u.nus.edu>
2025-01-18 17:41:02 +0800

    embed + query + retrieval

--- embedding.py
+++ embedding.py
@@ -8,3 +8,18 @@ def embed(input: str):
 
 embeddings = np.array([embed(chunk) for chunk in chunks])
 dimension - embeddings.shape[1]
+
+#query
+question = input("Please ask a question:")
+question_embeddings = np.array([embed(question)])
+
+#retrieval 
+D, I = index.search(question_embeddings, k=2) 
+distance_threshold = 0.7
+def retrieval():
+    if D[0][0] > distance_threshold:
+        return "Sorry, I don't know the answer."
+    else: 
+        retrieved_chunk = [chunks[i] for i in I.tolist()[0]]
+        return retrieved_chunk
+        
