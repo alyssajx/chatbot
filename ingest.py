a4fb5239c3b61f91b89338d5a41ce95832306006
sanya <sanghavisanya@gmail.com>
2025-01-18 22:56:04 +0800

    Trying to commit again

--- ingest.py
+++ ingest.py
@@ -2,28 +2,17 @@ import streamlit as st
 import os 
 import numpy as np
 import pandas as pd
+import faiss
 
-
+#langchain imports
 from langchain_community.document_loaders import NotionDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.schema.document import Document
 
 from openai import OpenAI
 
 client = OpenAI()
-
-#completion = client.chat.completions.create(
- # model="gpt-4o-mini",
-  #store=True,
-  #messages=[
-   # {"role": "user", "content": "write a haiku about ai"}
-  #],
-  #max_tokens = 100
-#)'''
-
-#print(completion.choices[0].message);
 
 def load_documents(): 
     document_loader = NotionDirectoryLoader("notion_content")
@@ -37,18 +26,21 @@ def split_documents(documents: list[Document]):
         chunk_overlap=100)
 
     return text_splitter.split_documents(documents)
+
 #Embed documents into vector space
 curr = load_documents()
 
 chunks = split_documents(curr)
 
+#Convert chunks into vectors embeddings  
+def get_embedding(text, model='text-embedding-ada-002'):
+    return client.embeddings.create(input=text, model=model)
+
 # Extract text content from the chunks
 text_chunks = [chunk.page_content for chunk in chunks]
+embeddings_array = np.array(get_embedding(text_chunks)).astype('float32')
 
-#Convert chunks into vectors embeddings 
-embeddings = client.embeddings.create(input=text_chunks, model='text-embedding-ada-002')
-#db = FAISS.from_documents(chunks, embeddings)
-#db.save_local("faiss_index")
+
 
 
 
